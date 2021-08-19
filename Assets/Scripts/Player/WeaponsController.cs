612b417a24aaa8fd5c671a86b30cb9e158e87cdc
Lautaro Coccia <lautaroauditore@gmail.com>
2021-08-18 22:16:13 -0300

    Add player & WeaponsController

---
+++ Assets/Scripts/Player/WeaponsController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponsController : MonoBehaviour
+{
+    Camera mainCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Shot();
+    }
+
+    void Shot()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePosition2D = new Vector2(mousePosition.x, mousePosition.y);
+            RaycastHit2D hit = Physics2D.Raycast(mousePosition2D, Vector2.zero);
+            if (hit.collider != null)
+            {
+                Debug.Log("hit");
+            }
+        }
+    }
+}
