080e378dfe1ac9698c6835f713c002cb9a350ae0
Lautaro Coccia <lautaroauditore@gmail.com>
2021-08-19 15:42:14 -0300

    Add basic reload system

--- Assets/Scripts/Player/WeaponsController.cs
+++ Assets/Scripts/Player/WeaponsController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class WeaponsController : MonoBehaviour
 {
+    [SerializeField] int ammo;
+    [SerializeField] int maxAmmo;
     Camera mainCamera;
     // Start is called before the first frame update
     void Start()
@@ -15,11 +17,12 @@ public class WeaponsController : MonoBehaviour
     void Update()
     {
         Shot();
+        Reload();
     }
 
     void Shot()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && ammo>0)
         {
             Vector3 mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
             Vector2 mousePosition2D = new Vector2(mousePosition.x, mousePosition.y);
@@ -28,6 +31,15 @@ public class WeaponsController : MonoBehaviour
             {
                 hit.transform.gameObject.GetComponent<IHitable>().OnHit();
             }
+            ammo--;
+        }
+    }
+
+    void Reload()
+    {
+        if(ammo == 0 && Input.GetKeyDown(KeyCode.R))
+        {
+            ammo = maxAmmo;
         }
     }
 }
