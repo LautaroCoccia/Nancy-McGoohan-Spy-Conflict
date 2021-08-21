a9da29f38c98d77e08a7cb4623b3faa999a6c505
Lautaro Coccia <lautaroauditore@gmail.com>
2021-08-21 19:53:58 -0300

    Add basic Hud & ammo UI

--- Assets/Scripts/Player/WeaponsController.cs
+++ Assets/Scripts/Player/WeaponsController.cs
@@ -1,25 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WeaponsController : MonoBehaviour
 {
     [SerializeField] int ammo;
     [SerializeField] int maxAmmo;
     Camera mainCamera;
+    public static Action<float> UpdateUIAmmo;
+    public static Action ResetUIAmmo;
     // Start is called before the first frame update
     void Start()
     {
         mainCamera = Camera.main;
     }
-
     // Update is called once per frame
     void Update()
     {
         Shot();
         Reload();
     }
-
     void Shot()
     {
         if (Input.GetMouseButtonDown(0) && ammo>0)
@@ -32,14 +34,15 @@ public class WeaponsController : MonoBehaviour
                 hit.transform.gameObject.GetComponent<IHitable>().OnHit();
             }
             ammo--;
+            UpdateUIAmmo?.Invoke(maxAmmo);
         }
     }
-
     void Reload()
     {
         if(ammo == 0 && Input.GetKeyDown(KeyCode.R))
         {
             ammo = maxAmmo;
+            ResetUIAmmo?.Invoke();
         }
     }
 }
---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] Image UIAmmo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        WeaponsController.UpdateUIAmmo += UpdateAmmo;
+        WeaponsController.ResetUIAmmo += ResetAmmo;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void UpdateAmmo(float actualAmmo)
+    {
+        float ammo = 1 / actualAmmo;
+        UIAmmo.fillAmount -= ammo;
+    }
+    void ResetAmmo()
+    {
+        UIAmmo.fillAmount = 1;
+    }
+    private void OnDisable()
+    {
+        WeaponsController.UpdateUIAmmo -= UpdateAmmo;
+        WeaponsController.ResetUIAmmo -= ResetAmmo;
+    }
+}
