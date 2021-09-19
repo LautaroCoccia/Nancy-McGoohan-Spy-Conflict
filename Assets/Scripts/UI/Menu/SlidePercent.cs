7e60f23aa7c9ee4540189cdb38241dba2ea749ad
SlayerWx <maxi_cs_95@hotmail.com>
2021-09-19 20:35:03 -0300

    slider implementation to use with sounds

---
+++ Assets/Scripts/UI/Menu/SlidePercent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class SlidePercent : MonoBehaviour
+{
+    [SerializeField] Slider mySlide;
+    [SerializeField] TMP_Text text;
+    void Start()
+    {
+        Change();
+    }
+    public void Change()
+    {
+         text.text = mySlide.value.ToString();
+    }
+}
