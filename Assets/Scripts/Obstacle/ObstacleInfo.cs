f964bc4d4f070eee1426cbcb8cffaf2d98e5c6b8
SlayerWx <maxi_cs_95@hotmail.com>
2021-09-21 18:21:11 -0300

    ObstacleInfo created

---
+++ Assets/Scripts/Obstacle/ObstacleInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleInfo : MonoBehaviour
+{
+    Transform coverPosition;
+    [SerializeField] Transform[] shootPosition;
+    public Vector3 GetCoverPosition => coverPosition.position;
+    public int GetShootPositionLength => shootPosition.Length;
+    private void Start()
+    {
+        coverPosition = GetComponent<Transform>();
+    }
+
+    public Vector3 GetShootPosition(int index)
+    {
+        return shootPosition[index].position;
+    }
+}
