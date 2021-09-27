beeafa7b5f9e4ee94eab89e1019cf52899d675ad
SlayerWx <maxi_cs_95@hotmail.com>
2021-09-27 09:13:28 -0300

    changed odds of spawn enemies

--- Assets/Scripts/BasicEnemy/EnemySpawner.cs
+++ Assets/Scripts/BasicEnemy/EnemySpawner.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEditor;
 public class EnemySpawner : MonoBehaviour
 {
+    [SerializeField] int probNormalSpawn; //prob+prob+prob = 100%
+    [SerializeField] int probShieldSpawn; // no necesitan sumar 100 en conjunto.
+    [SerializeField] int probScaredSpawn;
+
     [SerializeField] List<ObstacleInfo> barrelPositions; //TODO informacion del nivel, LevelManager
     [SerializeField] static int enemiesAlive;
     [SerializeField] int maxEnemiesAlive;
@@ -15,7 +19,6 @@ public class EnemySpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
     }
     // Update is called once per frame
     void Update()
@@ -33,7 +36,22 @@ public class EnemySpawner : MonoBehaviour
     }
     void SpawnEnemy()
     {
-        GameObject newEnemy = Instantiate(enemyPrefab[Random.Range(0, enemyPrefab.Count)]);
+        int aux = Random.Range(0, probNormalSpawn + probShieldSpawn + probScaredSpawn);
+        switch (aux)
+        {
+            case int _ when aux < probNormalSpawn:
+                aux = 0;
+                break;
+            case int _ when aux >= probNormalSpawn && aux < probNormalSpawn + probShieldSpawn:
+                aux = 1;
+                break;
+            case int _ when aux >= probNormalSpawn + probShieldSpawn &&
+            aux < probNormalSpawn + probShieldSpawn + probScaredSpawn:
+                aux = 2;
+                break;
+        }
+
+        GameObject newEnemy = Instantiate(enemyPrefab[aux]);
         newEnemy.transform.position = new Vector3(transform.position.x, transform.position.y, newEnemy.transform.position.z);
         newEnemy.gameObject.GetComponent<BaseEnemy>().SetObstaclesList(barrelPositions);
         enemiesAlive++;
