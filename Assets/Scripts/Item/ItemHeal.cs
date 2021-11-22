584c307141448614decadc96d44f2cc3ced8ff49
Lautaro Coccia <lautaroauditore@gmail.com>
2021-11-22 11:44:00 -0300

    Add medkit sounds

--- Assets/Scripts/Item/ItemHeal.cs
+++ Assets/Scripts/Item/ItemHeal.cs
@@ -6,8 +6,13 @@ public class ItemHeal : BasicItem
 {
     [SerializeField] int healPower;
     public static Action<int> OnHealPlayer;
+    private void OnEnable()
+    {
+        AkSoundEngine.PostEvent("medkit_spawn", gameObject);
+    }
     public override void OnHit(Weapon.DamageInfo damageInfo)
     { 
+        AkSoundEngine.PostEvent("medkit_heal", gameObject);
         OnHealPlayer?.Invoke(healPower);
         Destroy(gameObject);
     }
