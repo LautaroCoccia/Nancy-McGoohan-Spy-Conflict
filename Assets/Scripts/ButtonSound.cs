8c28f32a265655a3321106b89ba00dd1aff4a1b4
Lautaro Coccia <lautaroauditore@gmail.com>
2021-11-22 12:40:49 -0300

    Add buttons sounds

---
+++ Assets/Scripts/ButtonSound.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class ButtonSound : MonoBehaviour
+{
+    public void OnButtonPressed()
+    {
+        AkSoundEngine.SetSwitch("button", "button_press", gameObject);
+        AkSoundEngine.PostEvent("button", gameObject);
+    }
+    public void OnButtonStart()
+    {
+        AkSoundEngine.SetSwitch("button", "button_start", gameObject);
+        AkSoundEngine.PostEvent("button", gameObject);
+    }
+}
