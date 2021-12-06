8f1d8f67223800da62061becd2a6d1e6dca7cc9d
Lautaro Coccia <lautaroauditore@gmail.com>
2021-12-06 18:57:39 -0300

    Update enemies animations

--- Assets/Scripts/BasicEnemy/BaseEnemy.cs
+++ Assets/Scripts/BasicEnemy/BaseEnemy.cs
@@ -31,6 +31,7 @@ public class BaseEnemy : StateEnemy
     // Start is called before the first frame update
     protected override void Start()
     {
+        //animator = gameObject.GetComponent<Animator>();
         //animator.SetBool("IsMoving", true);
         base.Start();
         intermediateMove = false;
@@ -43,6 +44,8 @@ public class BaseEnemy : StateEnemy
     }
     protected override IEnumerator Choice()
     {
+        animator.SetTrigger("Idle");
+
         //animator.SetBool("IsMoving", false);
         choising = true;
         yield return new WaitForSeconds(choisingTime);
@@ -61,12 +64,15 @@ public class BaseEnemy : StateEnemy
                 {
                     state = State.uncover;
                     SelectUncoverPosition(barrelPositions[transformIndex].shootPosition);
+                    animator.SetTrigger("MoveRight");
                     //animator.SetBool("IsMoving", true);
                 }
                 break;
             case int n when n < (probToShoot + probToSpecial) && n> probToShoot
             && !switchTimerVsProbSpecial && probToSpecial!=0:
                 state = State.specialAction;
+                animator.SetTrigger("MoveRight");
+
                 //animator.SetBool("IsMoving", true);
                 break;
         }
@@ -75,8 +81,9 @@ public class BaseEnemy : StateEnemy
     void SelectCoverAndMove()
     {
         state = State.move;
+        
         SelectCoverPosition(barrelPositions);
-        //animator.SetBool("IsMoving", true);
+
     }
     void LostCover()
     {
@@ -100,14 +107,17 @@ public class BaseEnemy : StateEnemy
     }
     protected override void Move()
     {
+        animator.SetTrigger("MoveRight");
         transform.position = Vector3.MoveTowards(transform.position, nextPos, speed * Time.deltaTime);
         if (transform.position == nextPos && !intermediateMove)
         {
             state = State.choice;
+            animator.SetTrigger("Idle");
         }
         else if(transform.position == nextPos && intermediateMove)
         {
             SelectIntermediate();
+            
         }
     }
     void SelectIntermediate()
@@ -141,10 +151,15 @@ public class BaseEnemy : StateEnemy
     protected override void Uncover()
     {
         transform.position = Vector3.MoveTowards(transform.position, nextPos, speed * Time.deltaTime);
-        if (transform.position == nextPos) state = State.shoot;
+        if (transform.position == nextPos)
+        {
+            animator.SetTrigger("Idle");
+            state = State.shoot;
+        }
     }
     protected override IEnumerator Shoot()
     {
+        animator.SetTrigger("Shoot");
         //animator.SetBool("IsMoving", false);
         //OnShoot?.Invoke();
         yield return new WaitForSeconds(timeToWaitAndShoot);
@@ -157,13 +172,15 @@ public class BaseEnemy : StateEnemy
         }
         flashInWeapon.enabled = false;
         yield return new WaitForSeconds(timeWaitAndCover);
+        
         shooting = false;
+        animator.SetTrigger("Idle");
         state = State.Cover;
-        //OnIdle?.Invoke();
         //animator.SetBool("IsMoving", true);
     }
     protected override void Cover()
     {
+        //animator.SetTrigger("Idle");
         SelectActualCoverPosition();
         Move();
     }
