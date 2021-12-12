bf124ca64a35632826d88c347efa47c08c41e2f7
Lautaro Coccia <lautaroauditore@gmail.com>
2021-12-12 19:07:29 -0300

    Fix Settings menu

--- Assets/Scripts/Manager/MusicController.cs
+++ Assets/Scripts/Manager/MusicController.cs
@@ -5,22 +5,12 @@ using System;
 public class MusicController : MonoBehaviour
 {
     [SerializeField] List<string> listOfEvents;
-    [SerializeField] List<string> soundKeys;
-    [SerializeField] List<string> rtpcName;
     // Start is called before the first frame update
     void Start()
     {
         for (int i = 0; i < listOfEvents.Count ; i++)
         {
             AkSoundEngine.PostEvent(listOfEvents[i], gameObject);
-        }
-
-        for (int i = 0; i < soundKeys.Count ; i++)
-        {
-            if(PlayerPrefs.HasKey(soundKeys[i]))
-            {
-                AkSoundEngine.SetRTPCValue(rtpcName[i],PlayerPrefs.GetFloat(soundKeys[i]));
-            }
         }
     }
     public void StopAllSounds()
