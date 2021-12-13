2192b1af86f9a585a8348bd97cc288d3ea2b9509
SlayerWx <maxi_cs_95@hotmail.com>
2021-12-13 03:30:40 -0300

    Revert "Merge remote-tracking branch 'origin/Maxi' into Taro"
    This reverts commit f3760be325c5c68f18a8d03ecf1028802c79da9c, reversing
    changes made to 5c85353c022e504607832cf5f3019639781f081f.

--- Assets/Scripts/SaveSystem.cs
+++ Assets/Scripts/SaveSystem.cs
@@ -1,49 +1,33 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using System.Runtime.Serialization.Formatters.Binary;
+﻿using UnityEngine;
 using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
-
-[System.Serializable]
-public struct DataSave
+public static class SaveSystem
 {
-    public float saveFloat;
-    public string saveString;
-}
-public class SaveSystem : MonoBehaviour
-{
-    public static void SaveArchive(float i, string str)
+    public static void SaveArchive(DataSave data,string archiveName)
     {
-        DataSave data;
-        data.saveFloat = i;
-        data.saveString = str;
-        BinaryFormatter fm = new BinaryFormatter();
-        string path = Application.persistentDataPath + "/" + str + ".dat";
-        FileStream s = new FileStream(path, FileMode.Create);
-        s.SetLength(0);
-        fm.Serialize(s, data);
-        s.Close();
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/" + archiveName + ".dat";
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        formatter.Serialize(stream, data);
+        stream.Close();
     }
-    public static DataSave LoadArchive(string str)
+
+    public static DataSave LoadArchive(string archiveName)
     {
-        string path = Application.persistentDataPath + "/" + str + ".dat";
+        string path = Application.persistentDataPath + "/"+archiveName+".dat";
         if (File.Exists(path))
         {
-            BinaryFormatter fm = new BinaryFormatter();
-            FileStream s = new FileStream(path, FileMode.Open);
-            if (s.Length != 0)
-            {
-
-                DataSave data = (DataSave)fm.Deserialize(s);
-                s.Close();
-                return data;
-            }
-            s.Close();
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+            DataSave data = (DataSave)formatter.Deserialize(stream);
+            stream.Close();
+            return data;
         }
-        DataSave aux;
-        aux.saveString = "NULL";
-        aux.saveFloat = 0;
-        return aux;
+        else
+        {
+            return null;
+        }
     }
 }
