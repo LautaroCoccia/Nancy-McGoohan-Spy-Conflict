5c85353c022e504607832cf5f3019639781f081f
Lautaro Coccia <lautaroauditore@gmail.com>
2021-12-13 02:43:32 -0300

    Fix screen mode switch

--- Assets/Scripts/ScreenModeController.cs
+++ Assets/Scripts/ScreenModeController.cs
@@ -8,7 +8,7 @@ public class ScreenModeController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        SwitchScreenMode();
+       
     }
     public bool GetFullScreen() 
     {
