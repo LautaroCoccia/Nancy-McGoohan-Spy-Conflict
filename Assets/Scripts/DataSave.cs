5cd615592bddc7433b53cf02dca4e4bf31883639
Lautaro Coccia <lautaroauditore@gmail.com>
2021-12-13 02:54:49 -0300

    Revert "Merge remote-tracking branch 'origin/Maxi' into Taro"
    This reverts commit f3760be325c5c68f18a8d03ecf1028802c79da9c.

---
+++ Assets/Scripts/DataSave.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataSave : MonoBehaviour
+{
+    public float saveFloat;
+    public string saveString;
+    public DataSave(float i)
+    {
+        saveFloat = i;
+    }
+    public DataSave(string s)
+    {
+        saveString = s;
+    }
+    public DataSave(float i, string s)
+    {
+        saveFloat = i;
+        saveString = s;
+    }
+}
